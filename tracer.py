46311d17bd3b7c00ce6faf869c8b153ed9a0da40
Roddur Dasgupta <roddurd@gmail.com>
2019-06-15 13:26:30 -0700

    contour color fix

--- tracer.py
+++ tracer.py
@@ -2,31 +2,25 @@ import cv2
 import os
 import numpy as np
 
-img = cv2.imread("pfp3.jpg")
+img = cv2.imread("pfp.png")
 imgBW = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thres = cv2.threshold(imgBW, 30, 255, cv2.THRESH_BINARY)
+ret, thres = cv2.threshold(imgBW, 50, 255, cv2.THRESH_BINARY)
 kernel = np.ones((5,5),np.uint8) #square image kernel used for erosion
-erosion = cv2.erode(thres, kernel,iterations = 1) #refines all edges in the binary image
 
+erosion = cv2.erode(thres, kernel,iterations = 3) #refines all edges in the binary image
 opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
 closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel) #this is for further removing small noises and holes in the image
 
 contours, _ = cv2.findContours(closing,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #find contours with simple approximation
 
-biggest_contour = max(contours, key=cv2.contourArea)
-print("biggest contour: ", str(biggest_contour))
-cv2.drawContours(closing, [biggest_contour], 0, (0, 255, 0), 3)
-print("thres shape:", thres.shape)
-print("closing shape:", closing.shape)
-print("closing[0] shape:", closing[0].shape)
-print("biggest_contour shape:", biggest_contour.shape)
-print("biggest_contour[0] shape:", biggest_contour[0].shape)
+#test difference in biggest_contour & contours
+img_copy = img.copy()
+cv2.drawContours(img_copy, contours, -1, (255, 255, 0), cv2.FILLED)
 
-##closing[:] =255 
-#for i in range(closing.shape[0]):
-#	for j in range(closing.shape[1]):
-#		if [[i, j]] in biggest_contour:
-#			closing[i][j] = 0
-cv2.imshow('cleaner', closing) 
+biggest_contour = max(contours, key=cv2.contourArea)
+cv2.drawContours(img, [biggest_contour], -1, (0, 255, 0), cv2.FILLED)
+
+cv2.imshow('biggest', img) 
+cv2.imshow('contours', img_copy)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
