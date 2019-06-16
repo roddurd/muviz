bb33f930ac01e6a8e9590aa7d502494b9fa7fcb6
Roddur Dasgupta <roddurd@gmail.com>
2019-06-15 18:26:55 -0700

    centered zoom finally works

--- imgmanip.py
+++ imgmanip.py
@@ -9,16 +9,24 @@ img = cv2.imread("pfp3.jpg")
 def corners(img):
 	height, width, _ = img.shape
 	return [[0, 0], [width, 0], [0, height], [width, height]]
+def dim_corners(cnrs):
+	"""returns an np array (width, height) of a region bounded by corners in format [[x1, y1], ... [x4, y4]]"""
+	cnrs = np.array(cnrs)
+	return np.array([np.linalg.norm(cnrs[1]-cnrs[0]), np.linalg.norm(cnrs[2]-cnrs[0])])	
 def zoom_corners(img, percent_zoom):
+	height, width, _ = img.shape
 	cnrs = corners(img)
 	percent_zoom /= 100
 	cnrs = np.array(cnrs)
-	zoom_corners = [corner*(1 + percent_zoom) for corner in cnrs]
-	return zoom_corners	
+	zoom_cnrs = [corner/(1 + percent_zoom) for corner in cnrs]
+	offset = ((width - dim_corners(zoom_cnrs)[0])/2, ((height - dim_corners(zoom_cnrs)[1]))/2)
+	print("offset: ", offset)
+	zoom_cnrs = [corner+offset for corner in zoom_cnrs]	
+	return zoom_cnrs	
 def warp(img, pts1, pts2):
 	height, width, _ = img.shape
 	pts1, pts2 = map(np.float32, (pts1, pts2))
-	M = cv2.getPerspectiveTransform(pts1, pts2)
+	M = cv2.getPerspectiveTransform(pts2, pts1)
 	zimg = cv2.warpPerspective(img, M, (width, height))
 	return zimg
 def zoom(img, percent_zoom):
@@ -34,8 +42,10 @@ def color(img, color):
 	red_img = np.zeros_like(img)
 	red_img[:, :, index] = gray_img
 	return red_img
-
-img_zoom = zoom(img, 50)
+zoom_cnrs = zoom_corners(img, 75)
+print("zoom cnrs: ",str( zoom_cnrs)) #yay
+print(dim_corners(zoom_cnrs))
+img_zoom = zoom(img, 10)
 cv2.imshow('zoom', img_zoom)
 cv2.waitKey(0)
-
+print(str(dim_corners(corners(img))))
