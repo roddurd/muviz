61945aac431506f5f93a3512cac15b5f69026f45
Roddur Dasgupta <roddurd@gmail.com>
2019-06-16 18:06:31 -0700

    fracture img

--- imgmanip.py
+++ imgmanip.py
@@ -1,6 +1,8 @@
 import cv2 
 import os
 import numpy as np
+import re
+import random
 img = cv2.imread("pfp3.jpg")
 
 
@@ -46,7 +48,73 @@ def color(img, color):
 	red_img[:, :, index] = gray_img
 	return red_img
 
+#img fracture effect
+def fracture(img):
+	red=(0,0,255)
+	green=(0,255,0)
+	blue=(255,0,0)
+	height, width, _ = img.shape
+	center = (width/2, height/2)
+	polygons = []
+	for i in range(4):
+		left=[0,random.randint(20,height-20)]#left
+		top=[random.randint(20,width-20),0]#top
+		right=[width,random.randint(20,height-20)]#right
+		bottom=[random.randint(20,width-20),height]#bottom
+		polygons.append([left,top,center,[0,0]])
+		polygons.append([left,bottom,center,[0,height]])
+		polygons.append([right,top,center,[width,0]])
+		polygons.append([right,bottom,center,[width,height]])
+	for polygon in polygons:
+		clr = random.choice([red, green, blue])
+		cv2.fillPoly(img, np.int32([polygon]),clr) 	
+			
+
+
+#for file sorting
+def atoi(text):
+	try:
+		return int(text)
+	except:
+		return text
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    Otherwise, files are sorted like [1, 11, 2, 22, 3, etc]
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ] 
+
+
 #test/starter code
+beat_skip = 4
+
+proj_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(proj_dir, "data")
+output_dir = os.path.join(proj_dir, "output")
+
+"""
+for _, _, files in os.walk(output_dir):
+	files.sort(key=natural_keys)
+	for i, file in enumerate(files):
+		if not i%beat_skip:
+			print(file)
+			imgo = cv2.imread("output/"+file)
+			fracture(imgo)
+			cv2.imwrite("output/"+file,imgo)
+"""
+
+from vidsplice import Vidsplicer
+
+vid = Vidsplicer(output_dir)
+vid.join()					
+"""	
 img_zoom = zoom(img, 40)
 cv2.imshow('zoom', img_zoom)
 cv2.waitKey(0)
+"""
+
+
+
+
+
+
