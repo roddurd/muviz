e51a468263b52a8b7bfa13e7256ba8e47012d04e
Roddur Dasgupta <roddurd@gmail.com>
2019-06-25 12:32:22 -0700

    colors.get, framerate

--- imgmanip.py
+++ imgmanip.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import re
 import random
-img = cv2.imread("pfp3.jpg")
+img = cv2.imread("parrot.jpg")
 
 
 colors = {
@@ -199,98 +199,104 @@ colors = {
 
 #img zoom (warpPerspective)
 def corners(img):
-	height, width, _ = img.shape
-	return [[0, 0], [width, 0], [0, height], [width, height]]
+        height, width, _ = img.shape
+        return [[0, 0], [width, 0], [0, height], [width, height]]
 def dim_corners(cnrs):
-	"""returns an np array [width, height] of a region bounded by corners in format [[x1, y1], ... [x4, y4]]"""
-	cnrs = np.array(cnrs)
-	width = np.linalg.norm(cnrs[1]-cnrs[0])
-	height = np.linalg.norm(cnrs[2]-cnrs[0])
-	return np.array([width, height])	
+        """returns an np array [width, height] of a region bounded by corners in format [[x1, y1], ... [x4, y4]]"""
+        cnrs = np.array(cnrs)
+        width = np.linalg.norm(cnrs[1]-cnrs[0])
+        height = np.linalg.norm(cnrs[2]-cnrs[0])
+        return np.array([width, height])        
 def zoom_corners(img, percent_zoom):
-	"""returns an array, the set of corners comprising the box which is percent_zoom% zoomed into the center of the image, e.g., if percent_zoom is 100, then the width and height of img will be 100% bigger than the width and height of the box bounded by zoom_corners(img, 100)""" 
-	height, width, _ = img.shape
-	cnrs = corners(img)
-	percent_zoom /= 100
-	cnrs = np.array(cnrs)
-	zoom_cnrs = [corner/(1 + percent_zoom) for corner in cnrs]
-	offset = ((width - dim_corners(zoom_cnrs)[0])/2, ((height - dim_corners(zoom_cnrs)[1]))/2)
-	zoom_cnrs = [corner+offset for corner in zoom_cnrs]	
-	return zoom_cnrs	
+        """returns an array, the set of corners comprising the box which is percent_zoom% zoomed into the center of the image, e.g., if percent_zoom is 100, then the width and height of img will be 100% bigger than the width and height of the box bounded by zoom_corners(img, 100)""" 
+        height, width, _ = img.shape
+        cnrs = corners(img)
+        percent_zoom /= 100
+        cnrs = np.array(cnrs)
+        zoom_cnrs = [corner/(1 + percent_zoom) for corner in cnrs]
+        offset = ((width - dim_corners(zoom_cnrs)[0])/2, ((height - dim_corners(zoom_cnrs)[1]))/2)
+        zoom_cnrs = [corner+offset for corner in zoom_cnrs]     
+        return zoom_cnrs        
 def warp(img, pts1, pts2):
-	height, width, _ = img.shape
-	pts1, pts2 = map(np.float32, (pts1, pts2))
-	M = cv2.getPerspectiveTransform(pts2, pts1)
-	zimg = cv2.warpPerspective(img, M, (width, height))
-	return zimg
+        height, width, _ = img.shape
+        pts1, pts2 = map(np.float32, (pts1, pts2))
+        M = cv2.getPerspectiveTransform(pts2, pts1)
+        zimg = cv2.warpPerspective(img, M, (width, height))
+        return zimg
 def zoom(img, percent_zoom):
-	cnrs = corners(img)
-	zoom_cnrs = zoom_corners(img, percent_zoom)
-	return warp(img, cnrs, zoom_cnrs)
+        cnrs = corners(img)
+        zoom_cnrs = zoom_corners(img, percent_zoom)
+        return warp(img, cnrs, zoom_cnrs)
 
 #red,blue, green -scale
 def color(img, color):
-	"""returns img in red-, green-, or blue-scale depending on if color is 'red', 'blue', or 'green'"""
-	colors = {"blue":0, "green":1, "red":2}
-	index = colors.get(color)
-	gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	red_img = np.zeros_like(img)
-	red_img[:, :, index] = gray_img
-	return red_img
+        """returns img in red-, green-, or blue-scale depending on if color is 'red', 'blue', or 'green'"""
+        colors = {"blue":0, "green":1, "red":2}
+        index = colors.get(color)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        red_img = np.zeros_like(img)
+        red_img[:, :, index] = gray_img
+        return red_img
 
 #img fracture effect
 def fracture(img):
-	"""adds a slightly transparent set of polygons which all connect to the center, and each have a different color"""
-	red=(0,0,255)
-	green=(0,255,0)
-	blue=(255,0,0)
-	height, width, _ = img.shape
-	center = (width/2, height/2)
-	polygons = []
-	for i in range(10):
-		left=[0,random.randint(20,height-20)]#left
-		top=[random.randint(20,width-20),0]#top
-		right=[width,random.randint(20,height-20)]#right
-		bottom=[random.randint(20,width-20),height]#bottom
-		polygons.append([left,center,top,[0,0]])
-		polygons.append([left,center,bottom,[0,height]])
-		polygons.append([right,center,top,[width,0]])
-		polygons.append([right,center,bottom,[width,height]])
-	imgcopy = img.copy()
-	for polygon in polygons:
-		clr = random.choice(list(colors.values()))
-		cv2.fillPoly(imgcopy, np.int32([polygon]),clr) 	
-	alpha = 0.5
-	cv2.addWeighted(imgcopy, alpha, img, 1-alpha, 0, img)	
+        """adds a slightly transparent set of polygons which all connect to the center, and each have a different color"""
+        img = img.copy()
+        red=(0,0,255)
+        green=(0,255,0)
+        blue=(255,0,0)
+        height, width, _ = img.shape
+        center = (width/2, height/2)
+        polygons = []
+        for i in range(10):
+                left=[0,random.randint(20,height-20)]#left
+                top=[random.randint(20,width-20),0]#top
+                right=[width,random.randint(20,height-20)]#right
+                bottom=[random.randint(20,width-20),height]#bottom
+                polygons.append([left,center,top,[0,0]])
+                polygons.append([left,center,bottom,[0,height]])
+                polygons.append([right,center,top,[width,0]])
+                polygons.append([right,center,bottom,[width,height]])
+        imgcopy = img.copy()
+        for polygon in polygons:
+                clr = random.choice(list(colors.values()))
+                cv2.fillPoly(imgcopy, np.int32([polygon]),clr)  
+        alpha = 0.5
+        cv2.addWeighted(imgcopy, alpha, img, 1-alpha, 0, img)   
+        return img
 def tesselate(img, center, radius):
-	for i in range(-radius, radius):
-		for j in range(-radius, radius):
-			for k in range(5):
-				img[center[0]+radius+i][center[1]+k*radius+j]=img[center[0]-radius+i][center[1]-radius+j]
-	return img	
+        img = img.copy()
+        for i in range(-radius, radius):
+                for j in range(-radius, radius):
+                        for k in range(5):
+                                img[center[0]+radius+i][center[1]+k*radius+j]=img[center[0]-radius+i][center[1]-radius+j]
+        return img      
 def circle(img, center, radius, color='random',thickness=5,alpha=1):
-	img_copy = img.copy()
-	if color=='random':
-		clr = random.choice(list(colors.values()))
-	else:
-		clr = color
-	cv2.circle(img_copy, center, radius, clr,thickness) 
-	cv2.addWeighted(img_copy,alpha,img,1-alpha,0,img)
-	return img
+        img = img.copy()
+        img_copy = img.copy() #second copy for adding weighted onto img; for transparency
+        if color=='random':
+                clr = random.choice(list(colors.values()))
+        else:
+                clr = colors.get(color.upper())
+        cv2.circle(img_copy, center, radius, clr,thickness) 
+        cv2.addWeighted(img_copy,alpha,img,1-alpha,0,img)
+        return img
 def blur(img, intensity):
-	intensity += 1 if not intensity%2 else 0
-	cv2.GaussianBlur(img, (intensity, intensity), 3, img)
-	return img
+        img = img.copy()
+        intensity += 1 if not intensity%2 else 0
+        cv2.GaussianBlur(img, (intensity, intensity), 3, img)
+        return img
 def rotate(img, theta):
-	height, width, _ = img.shape
-	M = cv2.getRotationMatrix2D((width/2, height/2), theta,1)
-	img = cv2.warpAffine(img, M, (width, height))
-	return img	
+        img = img.copy()
+        height, width, _ = img.shape
+        M = cv2.getRotationMatrix2D((width/2, height/2), theta,1)
+        img = cv2.warpAffine(img, M, (width, height))
+        return img      
 
 
 #for file sorting
 def atoi(text):
-	return int(text) if text.isdigit() else text
+        return int(text) if text.isdigit() else text
 def natural_keys(text):
     '''
     alist.sort(key=natural_keys) sorts in human order
@@ -306,6 +312,7 @@ proj_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(proj_dir, "data")
 output_dir = os.path.join(proj_dir, "output")
 
+img = color(img, 'red')
 img = circle(img, (200, 400), 100, 'random', -1,0.4) 
 img = blur(img, 10)
 img = rotate(img, 45)
@@ -314,17 +321,17 @@ cv2.waitKey(0)
 
 """
 for _, _, files in os.walk(output_dir):
-	files.sort(key=natural_keys)
-	for i, file in enumerate(files):
-		if not i%beat_skip:
-			print(file)
-			imgo = cv2.imread("output/"+file)
-			fracture(imgo)
-			cv2.imwrite("output/"+file,imgo)
+        files.sort(key=natural_keys)
+        for i, file in enumerate(files):
+                if not i%beat_skip:
+                        print(file)
+                        imgo = cv2.imread("output/"+file)
+                        fracture(imgo)
+                        cv2.imwrite("output/"+file,imgo)
 from vidsplice import Vidsplicer
 
 vid = Vidsplicer(output_dir, fps=6)
-vid.join()					
+vid.join()                                      
 
 img_zoom = zoom(img, 40)
 cv2.imshow('zoom', img_zoom)
